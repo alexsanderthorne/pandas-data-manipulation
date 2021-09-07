215ff8db312669892ef1b9f5872c92f38ce40fb1
alexsanderthorne <alexsanderthorne@gmail.com>
2021-09-07 11:05:56 -0300

    add dataframes

---
+++ datasets.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('1/1/2000', periods=8)
+
+df = pd.DataFrame(np.random.randn(8, 4), index=dates, columns=['A', 'B', 'C', 'D'])
+
+#s = df['A']
+#s[dates[5]]
+
+df.loc[:, ['B','A']] = df[['A','B']].to_numpy()
+
+print(df)
+#print(s)
