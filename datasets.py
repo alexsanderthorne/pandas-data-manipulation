62bb52a3f0c9922c84e725be2f195ee6e8514a32
alexsanderthorne <alexsanderthorne@gmail.com>
2021-09-07 11:27:27 -0300

    setando uma coluna

--- datasets.py
+++ datasets.py
@@ -4,12 +4,13 @@ import numpy as np
 
 dates = pd.date_range('1/1/2000', periods=8)
 
-df = pd.DataFrame(np.random.randn(8, 4), index=dates, columns=['A', 'B', 'C', 'D'])
+df = pd.DataFrame(np.random.randn(8, 4), index=dates,
+                  columns=['A', 'B', 'C', 'D'])
 
 #s = df['A']
-#s[dates[5]]
+# s[dates[5]]
 
-df.loc[:, ['B','A']] = df[['A','B']].to_numpy()
+df.loc[:, ['B', 'A']] = df[['A', 'B']].to_numpy()
 
 print(df)
-#print(s)
+# print(s)
--- dict.py
+++ dict.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-x = pd.DataFrame({'x':[1,2,3], 'y':[3,4,5]})
+x = pd.DataFrame({'x': [1, 2, 3], 'y': [3, 4, 5]})
 print(x)
 
-x.iloc[1] = {'x':8, 'y':231}
+x.iloc[1] = {'x': 8, 'y': 231}
 print(x)
