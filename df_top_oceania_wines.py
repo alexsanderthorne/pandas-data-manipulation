ef4eac6df08a76727bc9b723a9e783cb3a991268
alexsanderthorne <alexsanderthorne@gmail.com>
2021-10-18 23:43:28 -0300

    atualizando df_top_oceania

--- df_top_oceania_wines.py
+++ df_top_oceania_wines.py
@@ -2,16 +2,12 @@ import pandas as pd
 
 wines = pd.read_csv('/home/studio/Downloads/winemag-data-130k-v2.csv')
 
-country = wines['country']
 df = pd.DataFrame(wines)
-nation = ['Australia']
-nation2 = ['New Zealand']
+nation = [ 'Australia','New Zealand']
 
-autraliaWines = df.loc[df['country'].isin(
-    nation)]
+oceania = df[(df['points'] > 95) & df['country'].isin(nation)]
+print(oceania.head(100))
 
-nzWines = df.loc[df['country'].isin(
-    nation2)]
 
-# print(autraliaWines)
-print(nzWines)
+# print(autraliaWines[0:95])
+# print(nzWines[0:95])
